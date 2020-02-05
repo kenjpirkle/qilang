56de95ff0bdc66570948b5f1e6471dcb5543dbe6
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-05 01:14:10 +0000

    Update main to test monoAllocator

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
-#include "bucketArray.hpp"
+#include "monoAllocator.hpp"
 #include <iostream>
+#include <array>
+#include <string>
+
+using namespace std::string_literals;
 
 auto main() -> int {
-    bucketArray<int, 16> ba;
-    ba.emplace(7);
-    std::cout << ba.size() << '\n';
+    monoAllocator<std::string, 1024> ma;
+    std::array<std::string*, 2048> arr;
+    for (int i = 0; i < 2048; ++i) {
+        arr[i] = &ma.emplace_back("hello " + std::to_string(i));
+    }
+    for (const auto& s : arr) {
+        std::cout << *s << '\n';
+    }
     return 0;
 }
