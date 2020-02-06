341a6b40a4169272fde3bc64f5f49a2d2485ebc2
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-06 12:19:54 +0000

    Changes to fString
    Replaced stringView_ struct with a std::string_view.
    Initial implementation of equality operators for fString.

--- includes/fString.hpp
+++ includes/fString.hpp
@@ -1,21 +1,43 @@
 #pragma once
 
+#include "typeDefinitions.hpp"
 #include <cstring>
 #include <vector>
-#include "typeDefinitions.hpp"
+#include <string_view>
 
 #pragma pack(push, 1)
 
+// fString acts as a union of a small buffer optimized string or a string_view
 struct fString {
 public:
+    inline auto size() const -> u64 {
+        return isSmall() ? internalStringSize() : stringView_.length;
+    }
+
+    inline auto operator ==(const fString& other) const -> bool {
+        if (size() != other.size()) {
+            return false;
+        } else if (capacity_.isSmall) {
+            return std::memcmp(
+                &internalString_,
+                &other.internalString_,
+                internalStringSize()) == 0;
+        } else {
+            return std::memcmp(
+                &stringView_[0],
+                &other.stringView_[0],
+                stringView_.size()) == 0;
+        }
+    }
+
+    inline auto operator !=(const fString& other) const -> bool {
+        return !(*this == other);
+    }
 private:
     std::size_t hash_;
     union {
         char internalString_[23];
-        struct {
-            const char* start;
-            u64 length;
-        } stringView_;
+        std::string_view stringView_;
     };
     struct smallCap {
         bool isSmall   : 1;
