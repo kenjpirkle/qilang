9dfd1b420b7a12f07420b5bbdd813fed8844245d
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-06 17:37:33 +0000

    Add tests for fString and associated ostream operator

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "fString.hpp"
 #include "monoAllocator.hpp"
 #include <iostream>
 #include <array>
@@ -6,6 +7,20 @@
 using namespace std::string_literals;
 
 auto main() -> int {
+    const std::array<fString, 10> fss = {
+        "a","b","c","d","e","f","g","hello!","i","j"
+    };
+    auto fs2 = fString("hello!");
+
+    for (const auto& f : fss) {
+        std::cout
+            << f
+            << " and "
+            << fs2
+            << " are "
+            << (f == fs2 ? "the same" : "not the same")
+            << '\n';
+    }
     monoAllocator<std::string, 1024> ma;
     std::array<std::string*, 2048> arr;
     for (int i = 0; i < 2048; ++i) {
