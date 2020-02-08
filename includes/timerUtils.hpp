73a1626ece68e947b375803038c92b0a9c8c3bce
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-08 12:04:49 +0000

    Modify bench function

--- includes/timerUtils.hpp
+++ includes/timerUtils.hpp
@@ -7,12 +7,11 @@
 
 namespace timerUtils {
     static inline auto bench(std::function<void()> f, const std::string& message) -> void {
-        std::cout << message << '\n';
+        std::cout << message << ": ";
         auto start = std::chrono::steady_clock::now();
         f();
         auto end = std::chrono::steady_clock::now();
         std::cout
-            << "finished: "
             << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
             << '\n';
     }   
