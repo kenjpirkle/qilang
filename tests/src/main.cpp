3614e23f391d0ee535945e531f1588ebe4aa3e7b
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-08 12:05:02 +0000

    Add tests

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -5,8 +5,12 @@ auto main() -> int {
     bucketArrayTest<1, 70000>();
     bucketArrayTest<70000, 70000>();
     bucketArrayTest<16, 444>();
+    bucketArrayTest<1024, 8192>();
+    bucketArrayTest<4096, 555>();
     monoAllocatorTest<1, 70000>();
     monoAllocatorTest<70000, 70000>();
     monoAllocatorTest<16, 444>();
+    monoAllocatorTest<1024, 8192>();
+    monoAllocatorTest<4096, 555>();
     return 0;
 }
