c78f2a61ed8ac943dbf32084442993b643ade386
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-08 18:57:00 +0000

    Add initial work on small buffer optimization in mono_allocator

--- includes/mono_allocator.hpp
+++ includes/mono_allocator.hpp
@@ -2,46 +2,64 @@
 
 #include <memory>
 #include <array>
+#include <variant>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 #pragma pack(push, 1)
 
-template <typename T, int N>
+template <typename T, int N, int SN = 0>
 struct mono_allocator {
 public:
     mono_allocator() {
-        curr_ = last_ = nullptr;
+        buffer_ = buffer_variant {
+            in_place_index_t<1>{},
+            large_buffer {
+                .curr = nullptr,
+                .last = nullptr,
+                .slabs = {}
+            }
+        };
     }
     ~mono_allocator() {
-        for (auto& s : slabs_) {
-            free(s);
+        for (auto& s : large().slabs) {
+            delete s;
         }
     }
     auto emplace_back(T&& element) -> T* {
-        if (curr_ == last_) {
+        if (large().curr == large().last) {
             allocate_slab();
         }
 
-        *curr_ = element;
-        T* ptr = curr_;
-        ++curr_;
+        *large().curr = element;
+        T* ptr = large().curr;
+        ++large().curr;
         return ptr;
     }
 
 private:
     using slab = array<T, N>;
+    using small_size_slabs = array<slab, SN>;
+    struct large_buffer {
+        T* curr;
+        T* last;
+        vector<slab*> slabs;
+    };
+    using buffer_variant = variant<small_size_slabs, large_buffer>;
+
+    buffer_variant buffer_;
 
     auto allocate_slab() -> void {
-        auto s = slabs_.emplace_back(new slab);
-        curr_ = &((*s)[0]);
-        last_ = &((*s)[N]);
+        auto s = large().slabs.emplace_back(new slab);
+        large().curr = &((*s)[0]);
+        large().last = &((*s)[N]);
     }
 
-    T* curr_;
-    T* last_;
-    vector<slab*> slabs_;
+    inline auto large() -> large_buffer& {
+        return get<1>(buffer_);
+    }
 };
 
 #pragma pack(pop)
