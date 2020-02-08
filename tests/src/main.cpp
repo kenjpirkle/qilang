7e47d52da9861e41f1d608a1ed94f9266f4421e4
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-08 18:58:25 +0000

    Add tests to tests/src/main.cpp

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,5 +1,8 @@
 #include "bucket_array_test.cpp"
 #include "mono_allocator_test.cpp"
+#include "mono_allocator.hpp"
+#include <iostream>
+#include <string>
 
 auto main() -> int {
     bucket_array_test<1, 70000>();
@@ -9,6 +12,8 @@ auto main() -> int {
     bucket_array_test<4096, 555>();
     mono_allocator_test<1, 70000>();
     mono_allocator_test<70000, 70000>();
+    mono_allocator_test<1, 2>();
+    mono_allocator_test<16, 443>();
     mono_allocator_test<16, 444>();
     mono_allocator_test<1024, 8192>();
     mono_allocator_test<4096, 555>();
