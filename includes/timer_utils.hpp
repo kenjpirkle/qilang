2b8b9e9aaa548c15a6e82ececdcbcc25eaf69a8d
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-08 18:57:20 +0000

    Move benchmark message to end

--- includes/timer_utils.hpp
+++ includes/timer_utils.hpp
@@ -8,11 +8,11 @@
 using namespace std;
 
 namespace timer_utils {
-    static inline auto bench(function<void()> f, const string& message) -> void {
-        cout << message << ": ";
+    static inline auto bench(const function<void()> f, const string& message) -> void {
         auto start = chrono::steady_clock::now();
         f();
         auto end = chrono::steady_clock::now();
+        cout << message << ": ";
         cout
             << chrono::duration_cast<chrono::milliseconds>(end - start).count()
             << '\n';
