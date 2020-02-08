706d30b10f51afe664e4871db32d816d4a316a95
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-08 18:58:07 +0000

    Add small modification in parser class

--- src/parser.cpp
+++ src/parser.cpp
@@ -8,5 +8,7 @@ parser::parser(compile_context* context) :
 
 auto parser::watch_for_modules() -> void {
     optional<pair<string_view, string_view>> file_module;
-    while (!context_->cancelled())
+    do {
+        file_module = context_->pop();
+    } while (!context_->cancelled());
 }
