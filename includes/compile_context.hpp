3d51ae689338c56c695d85f570c75dfe3ac09de1
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-12 13:53:00 +0000

    Update compile_context.hpp
    Replaces std::string with f_string.
    Removes vector of threads (each parser now uses a local thread).

--- includes/compile_context.hpp
+++ includes/compile_context.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
-#include "type_definitions.hpp"
+#include "f_string.hpp"
 #include "mono_allocator.hpp"
+#include "parser.hpp"
+#include "thread_state.hpp"
+#include "type_definitions.hpp"
 #include <unordered_map>
 #include <mutex>
 #include <string_view>
@@ -11,24 +14,28 @@
 
 using namespace std;
 
+struct parser;
+
 #pragma pack(push, 1)
 
 struct compile_context {
 public:
-    compile_context(const string_view);
-    auto add(const string_view) -> void;
-    auto pop() -> pair<string_view, string_view>;
-    auto cancelled() -> bool;
-    auto empty() -> bool;
+    compile_context(const f_string<23>);
+    auto add(const f_string<23>) -> void;
+    auto pop() -> pair<f_string<23>, string_view>;
+    auto cancel() -> void;
+    auto cancelled() const -> bool;
+    auto finished() const -> bool;
+    auto empty() const -> bool;
+    auto contains(const f_string<23>) const -> bool;
 
 private:
     mutex mutex_;
     thread::id main_thread_id_;
-    vector<thread> threads_;
-    vector<bool> thread_states_;
-    queue<string_view> file_queue_;
-    mono_allocator<string, 16> module_alloc_;
-    unordered_map<string_view, string*> modules_;
+    vector<parser> parsers_;
+    queue<f_string<23>> file_queue_;
+    mono_allocator<f_string<23>, 16> module_alloc_;
+    unordered_map<f_string<23>, string*, f_string_hasher<23>> modules_;
 };
 
 #pragma pack(pop)
--- src/compile_context.cpp
+++ src/compile_context.cpp
@@ -1,32 +1,57 @@
 #include "compile_context.hpp"
+#include <algorithm>
 
 using namespace std;
 
-compile_context::compile_context(const string_view initial_file_path) {
+compile_context::compile_context(f_string<23> initial_file_path) {
     main_thread_id_ = this_thread::get_id();
-    threads_.reserve(thread::hardware_concurrency());
+    parsers_.reserve(thread::hardware_concurrency());
     file_queue_.push(initial_file_path);
 }
 
-auto compile_context::add(const string_view file_path) -> void {
+auto compile_context::add(const f_string<23> file_path) -> void {
     mutex_.lock();
     if (modules_.find(file_path) != modules_.end()) {
         file_queue_.push(file_path);
-        if (threads_.size() < thread::hardware_concurrency()) {
-            threads_.emplace_back(thread(
-                [&]() {
-                    // start a parser here
-                }
-            )).detach();
+        if (parsers_.size() < thread::hardware_concurrency()) {
+            parsers_.emplace_back(this);
         }
     }
     mutex_.unlock();
 }
 
-auto compile_context::pop() -> pair<string_view, string_view> {
+auto compile_context::pop() -> pair<f_string<23>, string_view> {
     return { "test_file", "test_module" };
 }
 
-auto compile_context::cancelled() -> bool {
-    return false;
+auto compile_context::cancel() -> void {
+
 }
+
+auto compile_context::cancelled() const -> bool {
+    return any_of(
+        parsers_.begin(),
+        parsers_.end(),
+        [](const auto& p) {
+            return p.thr_state == thread_state::cancelled;
+        }
+    );
+}
+
+auto compile_context::finished() const -> bool {
+    return all_of(
+        parsers_.begin(),
+        parsers_.end(),
+        [](const auto& p) {
+            return p.thr_state == thread_state::finished;
+        }
+    );
+}
+
+auto compile_context::empty() const -> bool {
+    return file_queue_.empty();
+}
+
+auto compile_context::contains(const f_string<23> fs) const -> bool {
+    return modules_.find(fs) != modules_.end();
+} 
