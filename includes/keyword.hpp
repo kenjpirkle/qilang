cbdfaed90f2446b0bea116b9a2ee69d462ef18aa
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-14 22:36:21 +0000

    Add keyword enum to project

---
+++ includes/keyword.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "type_definitions.hpp"
+
+enum keyword : u8 {
+    if_kw,
+    else_kw,
+    for_kw,
+    return_kw,
+    type_kw,
+    trait_kw,
+    enum_kw,
+    union_kw,
+    defer_kw
+};
