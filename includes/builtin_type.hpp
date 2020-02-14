216f014759f457937a230cf20f9bb38d6784b439
Ken Pirkle <kenjpirkle@gmail.com>
2020-02-14 22:36:10 +0000

    Add builtin_type enum to project

---
+++ includes/builtin_type.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "type_definitions.hpp"
+
+enum builtin_type : u8 {
+    int_t,
+    i8_t,
+    i16_t,
+    i32_t,
+    i64_t,
+    i128_t,
+    uint_t,
+    u8_t,
+    u16_t,
+    u32_t,
+    u64_t,
+    u128_t,
+    float_t,
+    f32_t,
+    f64_t,
+    f128_t,
+    bool_t,
+    char_t,
+    string_t
+};
